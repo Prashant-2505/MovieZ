d3100583c611744e622b215f2876b9583b62fc4a
Prashant-2505 <shramaprashant2505@gmail.com>
2023-04-17 17:53:25 +0530

    home page hero banner ready

--- src/App.jsx
+++ src/App.jsx
@@ -19,20 +19,29 @@ function App() {
 
 
   useEffect(() => {
-    apiTesting()
+    FetchApiConfig()
   }, [])
 
-  const apiTesting = () => {
-    fetchDataFromApi('/movie/popular')
+  const FetchApiConfig = () => {
+    fetchDataFromApi('/configuration')
       .then((res) => {
-        console.log(res)
-        dispatch(getApiConfiguration(res))
+        const url =
+        {
+          backdrop: res.images.secure_base_url + "original",
+
+          poster: res.images.secure_base_url + "original",
+
+          profile: res.images.secure_base_url + "original"
+
+
+        }
+        dispatch(getApiConfiguration(url))
       })
   }
 
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,23 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.scss'
+import { useNavigate } from 'react-router-dom'
+import useFetch from '../../../hooks/useFetch'
+import { useSelector } from 'react-redux'
+import Img from '../../../components/lazyLoadImage/Img'
+import ContentWrapper from '../../../components/contentWraper/ContentWrapper'
+
+
 
 const HeroBanner = () => {
+
+    const [background, setBackground] = useState('')
+    const [query, setQuery] = useState('')
+    const navigate = useNavigate()
+    const { url } = useSelector((state) => state.home)
+    const { data, loading } = useFetch('/movie/upcoming')
+    useEffect(() => {
+        // so here we want to add some image randomly when we load 
+        // no we got data then results of data have many results of 20 thats why we multiply it wiht 20 
+        // then we use math.random to get random data from result every time and it may be give some decimel thats why we useing floor with it 
+        // .backdrop path give background image 
+        const bg = url.backdrop + data?.results[Math.floor(Math.random() * 20)]?.backdrop_path
+        setBackground(bg)
+    }, [data])
+
+    const serachQueryHandler = (e) => {
+        if (e.key === 'Enter' && query.length > 0) {
+            navigate(`/serch/${query}`)
+        }
+    }
+
+
     return (
         <div className='heroBanner'>
-            <div className="wrapper">
+            {!loading && <div className="backdrop-img">
+                <Img src={background} />
+            </div>}
+
+            <div className="opacity-layer">
+
+            </div>
+
+            <ContentWrapper>
                 <div className="heroBannerContent">
                     <span className="title"> Welcome.</span>
                     <span className="subTitle">Millions of movies, Tv shows and people to discover.
                         Explore now.</span>
+
+                    <div className="searchInput">
+                        <input
+                            type="text"
+                            placeholder='Search for movie or tv shows'
+                            onChange={(e) => setQuery(e.target.value)}
+                            onKeyUp={serachQueryHandler}
+                        />
+                        <button>Search</button>
+                    </div>
                 </div>
-                <div className="searchInput">
-                    <input
-                        type="text"
-                        placeholder='Search for movie or tv shows'
-                    />
-                    <button>Search</button>
-                </div>
-            </div>
+
+            </ContentWrapper>
         </div>
     )
 }
