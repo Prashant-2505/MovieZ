2c1bb1623e95a5968964009474434bc57a45dc2f
Prashant-2505 <shramaprashant2505@gmail.com>
2023-04-20 12:16:21 +0530

    Trending caousel done

--- src/components/switchTab/SwitchTab.jsx
+++ src/components/switchTab/SwitchTab.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react'
 import './style.scss'
-const SwitchTab = ({data,onChangeTab}) => {
+const SwitchTab = ({ data, onTabChange }) => {
 
-    const [selectedTab, setSelectedTab] = useState(0)
-    const [left,setLeft]= useState(0)
-    const activeTab = (tab,index)=>
-    {
-         setLeft(index*100)
-         setTimeout(()=>
-         {
-            setSelectedTab(index)
-         },300)
-         onTabChange(tab,index)
-    }
+  const [selectedTab, setSelectedTab] = useState(0)
+  const [left, setLeft] = useState(0)
+  const activeTab = (tab, index) => {
+    setLeft(index * 100)
+    setTimeout(() => {
+      setSelectedTab(index)
+    }, 300)
+    onTabChange(tab, index)
+  }
   return (
-    <div className='switchingabs'>
-       <div className="tabItems">
-         {data.map((tab,index)=>
-         (
-            <span key={index} className={`tabItem`} >{tab}</span>
-         ))}
-         <span className="movingBg" style={{left}} ></span>
-       </div>
+    <div className='switchingTabs'>
+      <div className="tabItems">
+        {data.map((tab, index) =>
+        (
+          <span
+            onClick={() => activeTab(tab, index)}
+            key={index} className={`tabItem ${selectedTab === index ? 'active' : ''}`} >{tab}</span>
+        ))}
+        <span className="movingBg" style={{ left }} ></span>
+      </div>
     </div>
   )
 }
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ContentWrapper from '../../../components/contentWraper/ContentWrapper'
 import SwitchTab from '../../../components/switchTab/SwitchTab'
+import useFetch from '../../../hooks/useFetch'
+import Carousel from '../../../components/carousel/Carousel'
 
- const onTabChange = (tab)=>
- {
-
- }
 
 const Trending = () => {
+
+
+    const [endPoint, setEndPont] = useState('day')
+    const {data, loading} = useFetch(`/trending/all/${endPoint}`)
+    const onTabChange = (tab) => {
+        setEndPont(tab==='day'?'day':'week')
+    }
+
     return (
         <div className='carouselSection'>
-            <ContentWrapper>
+            <ContentWrapper >
                 <span className="carouselTitle">Trending</span>
-                <SwitchTab data={["Day, Week"]} onTabChange={onTabChange}/>
+                <SwitchTab data={["day", "week"]} onTabChange={onTabChange} />
             </ContentWrapper>
+
+            <Carousel data={data?.results} loading={loading}/>
         </div>
     )
 }
